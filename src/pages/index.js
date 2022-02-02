8c7ae2722e0c22a678a0dea83d6f6187b98af672
Gabe Kelley <hello@gabekelley.com>
2022-02-01 19:24:32 -0500

    updated bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,13 +19,12 @@ class IndexPage extends React.Component {
           <div className="bio-wrap">
             <div className="bio-wrap__top">
               <h1 className="thin">Gabe Kelley</h1>
-              <h2 className="light">Currently a Staff Product Designer at Etsy<sup><a href="#fn1" id="ref1">1</a></sup> in Brooklyn,  I'm focused on design systems, service design and operations. As a human-centered designer, I believe I have a duty to only build with organizations that aim to improve the lives of others. While not at work, I'm either reading<sup><a href="#fn2" id="ref2">2</a></sup>, pursuing various other interests<sup><a href="#fn3" id="ref3">3</a></sup>, or finding ways to fight inequality. If you'd like to chat, go on and send me <a href="mailto:gabe@hey.com">an email</a>.</h2>
+              <h2 className="light">Currently a Staff Product Designer at Etsy<sup><a href="#fn1" id="ref1">1</a></sup>,  I'm focused on design systems, service design and operations. As a human-centered designer, I believe I have a duty to only build with organizations that aim to improve the lives of others. In my free time, I'm reading<sup><a href="#fn2" id="ref2">2</a></sup>.If you'd like to chat, go on and send me <a href="mailto:hello@gabekelley.com">an email</a>.</h2>
             </div>
             <div className="bio-wrap__bottom thin">
               <div className="divider"></div>
               <sup>1</sup> <span>2015 —</span> <a href="#ref1" id="fn1">↩</a><br/>
-              <sup>2</sup> <span>Currently <span className="italic">Capital and Ideology</span> by Thomas Piketty</span> <a href="#ref2" id="fn2">↩</a><br/>
-              <sup>3</sup> <span>Cycling, photography, camping, and then probably reading some more.</span> <a href="#ref3" id="fn3">↩</a><br/>
+              <sup>2</sup> <span>Currently <span className="italic">Devil House</span> by John Darnielle</span> <a href="#ref2" id="fn2">↩</a><br/>
             </div>
           </div>
         </div>
